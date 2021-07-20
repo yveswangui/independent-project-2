35df6c5c08304f3ae03336bb9daf2db1fddd23d6
yvonnewangui <yvonne.wamburu@student.moringaschool.com>
2021-07-20 11:24:47 +0300

    add control flows with or operator for date and month

--- js/index.js
+++ js/index.js
@@ -2,18 +2,23 @@
 var form = document.getElementById("form")
 
 maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
     var DD = document.getElementById("date").value
     var MM = document.getElementById("month").value
     var userYear = document.getElementById("year").value
     var man = document.getElementById("male")
     e.preventDefault()
 
-    if(DD <= 0 || DD > 31) {
-        alertMonth()
+    if (DD <= 0 || DD > 31) {
+        alertDate()
         return
+    } else {
+        //console.log(man.checked)
+    }
+    if (MM <= 0 || MM > 12) {
+        alertMonth()
     } else {
         //console.log(woman.checked)
     }
@@ -35,21 +40,31 @@ form.addEventListener("submit", function(e){
 
     console.log(roundedDay)
 
-    if(man.checked == true){
-        var bornDay = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+    if (man.checked == true) {
+        var bornDay = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
         var key = parseInt(bornDay)
         console.log(maleNames[key])
         document.getElementById("output").innerHTML = maleNames[key]
     }
 })
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
     var DD = document.getElementById("date").value
     var MM = document.getElementById("month").value
     var userYear = document.getElementById("year").value
-
     var woman = document.getElementById("female")
     e.preventDefault()
-
+    if (DD <= 0 || DD > 31) {
+        alertDate()
+        return
+    } else {
+        //console.log(woman.checked)
+    }
+    if (MM <= 0 || MM > 12) {
+        alertMonth()
+        return
+    } else {
+        //console.log(woman.checked)
+    }
     var newYear = userYear.toString()
 
     // console.log(newYear)
@@ -62,14 +77,17 @@ form.addEventListener("submit", function(e){
     var YY = firstThird + firstFourth
 
 
-    if(woman.checked == true){
-        var bornDay =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7
+    if (woman.checked == true) {
+        var bornDay = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
         var key = parseInt(bornDay)
         console.log(femaleNames[key])
         document.getElementById("output").innerHTML = femaleNames[key]
 
     }
+    function alertDate() {
+        alert("Date is invalid")
+    }
+})
 function alertMonth() {
-    alert("enter valid month")
+    alert("Month is invalid")
 }
-})  
